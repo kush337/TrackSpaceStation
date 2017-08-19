194a6bd7d53548857977e2e2f3b2177c221caf28
Unknown <kush_patel7@yahoo.com>
2017-08-19 14:55:03 -0400

    Added starter HTML page and update methods

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-	response = requests.get('http://api.open-notify.org/iss-now.json')
-	json_data = response.json()
-	json_dict = json.loads(json_data) 	
-	return json_dict
+def index(lat=None, lon=None):
+	iss_response = requests.get('http://api.open-notify.org/iss-now.json')
+	json_data = iss_response.content
+	json_dict = json.loads(json_data)
+
+	lat = json_dict['iss_position']['latitude']
+	lon = json_dict['iss_position']['longitude'] 	
+	return render_template('index.html', lat=lat, lon=lon)
 
 
 if __name__ == '__main__':
